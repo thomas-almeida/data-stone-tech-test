b25bf4a5b0fc5c7a0fb8efebf1ea40ea02104589
Thomas Almeida <thomas.almeida@tradeupgroup.com>
2024-03-06 19:07:30 -0300

    Criando o client base com vuetify

---
+++ client/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer
+        expand-on-hover
+        rail
+      >
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+            subtitle="sandra_a88@gmailcom"
+            title="Sandra Adams"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+
+          <v-list-item prepend-icon="mdi-account" title="Cadastrar novo Cliente" value="create-customer"></v-list-item>
+          <v-list-item prepend-icon="mdi-account" title="Cadastrar novo Produto" value="create-product"></v-list-item>
+          <v-list-item prepend-icon="mdi-account" title="Meus Clientes" value="list-customers"></v-list-item>
+          <v-list-item prepend-icon="mdi-account" title="Meus Produtos" value="list-products"></v-list-item>
+
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main style="height: 100vh">
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>

ba95586c1613eb6c3b2cc5e1b5a0af21d210dfcd
Thomas Almeida <thomas.almeida@tradeupgroup.com>
2024-03-07 01:08:08 -0300

    Refactoring HomeView

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,41 @@
+<template>
+  <main>
+    <h1>Visão Geral</h1>
+
+    <div class="dash-container all-flex gap-15">
+      <div class="dash-box all-flex">
+        <span>
+          <h2>Clientes na Carteira do Sistema</h2>
+          <h2>{{ getTotalCustomersComputed }}</h2>
+        </span>
+      </div>
+      <div class="dash-box all-flex">
+        <span>
+          <h2>Produtos Cadastrados</h2>
+          <h2> {{ getTotalProductsComputed }} </h2>
+        </span>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import { useCustomerStore } from '@/stores/Customer';
+import { useProductStore } from '@/stores/Product';
+import { mapGetters } from 'pinia';
+
+export default {
+  computed: {
+    ...mapGetters(useCustomerStore, ['getCustomerList']),
+    ...mapGetters(useProductStore, ['getProductList']),
+    
+    getTotalCustomersComputed() {
+      return this.getCustomerList.length
+    },
+    getTotalProductsComputed() {
+      return this.getProductList.length
+    }
+  }
+}
+
+</script>

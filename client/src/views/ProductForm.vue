eefee0801a9785111e758b4a7cea13c180b09b45
Thomas Almeida <thomas.almeida@tradeupgroup.com>
2024-03-06 23:59:10 -0300

    Creating second form version

---
+++ client/src/views/ProductForm.vue
@@ -0,0 +1,83 @@
+<template>
+  <main>
+    <h1>Cadastrar Novo Produto</h1>
+    <p>
+      Cadastre novos produtos aqui,
+      você será capaz de associar clientes a ele depois.
+    </p>
+
+    <form>
+
+      <span>
+        <label for="">Nome do Produto</label>
+        <input type="text" v-model="formData.nome" placeholder="Pacote de Leads I">
+      </span>
+      <span>
+        <label for="">Valor Mensal</label>
+        <input type="tel" v-model="formData.valor" placeholder="R$ 0.000,00">
+      </span>
+      <span>
+        <label for="">Desconto</label>
+        <input type="tel" v-model="formData.desconto" placeholder="%">
+      </span>
+      <span class="select-field">
+        <label for="">Status do Produto</label>
+        <select name="" id="" v-model="formData.status">
+          <option value="">Selecione</option>
+          <option value="ATIVO">Ativo</option>
+          <option value="INATIVO">Inativo</option>
+        </select>
+      </span>
+    </form>
+
+    <button 
+      class="btn-submit"
+      @click="submitForm"
+      :class="{ 'btn-disabled': !isValid() }"
+      :disabled="!isValid()"
+    >
+      Cadastrar Produto
+    </button>
+
+  </main>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      formData: {
+        nome: '',
+        valor: '',
+        desconto: '',
+        status: ''
+      },
+      statusOptions: ['Ativo', 'Inativo']
+    };
+  },
+  methods: {
+    submitForm() {
+      event.preventDefault()
+      console.log('Dados do formulário:', this.formData);
+      this.resetForm();
+    },
+    resetForm() {
+      this.formData = {
+        nome: '',
+        valor: '',
+        desconto: '',
+        status: ''
+      };
+    },
+    isValid() {
+      for (let key in this.formData) {
+        if (!this.formData[key]) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+};
+</script>
---
+++ client/src/views/ProductList.vue
@@ -0,0 +1,65 @@
+<template>
+    <main>
+        <h1>Meus Clientes</h1>
+        <p>
+            Cadastre novos clientes aqui,
+            você será capaz de associar produtos a ele depois.
+        </p>
+        <div>
+            <ListTable 
+                :headers="
+                [
+                    'Nome',
+                    'Valor',
+                    'Desconto', 
+                    'Status'
+                ]" 
+                :items="produtos"
+                :actions="acoesCliente" 
+            />
+        </div>
+    </main>
+</template>
+
+<script>
+import ListTable from '@/components/ListTable.vue';
+
+export default {
+    components: {
+        ListTable
+    },
+    data() {
+        return {
+            produtos: [
+                {
+                    id: 1,
+                    nome: 'PACOTE LEADS I',
+                    valor: 'R$ 2000',
+                    desconto: '10%',
+                    status: 'Ativo'
+                },
+                {
+                    id: 2,
+                    nome: 'PACOTE LEADS II',
+                    valor: 'R$ 2000',
+                    desconto: '10%',
+                    status: 'Ativo'
+                },
+                {
+                    id: 3,
+                    nome: 'PACOTE LEADS III',
+                    valor: 'R$ 2000',
+                    desconto: '10%',
+                    status: 'Ativo'
+                },
+                // Adicione mais itens conforme necessário
+            ],
+            acoesCliente: [
+                { label: 'Editar', action: 'editar' },
+                { label: 'Excluir', action: 'excluir' },
+                // Adicione mais ações conforme necessário
+            ]
+        };
+    }
+};
+</script>

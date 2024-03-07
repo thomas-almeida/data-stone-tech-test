200c4542475b2e0ec5352b9e008beba528f8e8c0
Thomas Almeida <thomas.almeida@tradeupgroup.com>
2024-03-06 23:58:34 -0300

    Creating first forms version

---
+++ client/src/views/CustomerForm.vue
@@ -0,0 +1,94 @@
+<template>
+  <main>
+    <h1>Cadastrar Novo Cliente</h1>
+    <p>
+      Cadastre novos clientes aqui,
+      você será capaz de associar produtos a ele depois.
+    </p>
+
+    <form>
+
+      <span>
+        <label for="">Nome</label>
+        <input type="text" v-model="formData.nome" placeholder="João da Silva">
+      </span>
+      <span>
+        <label for="">CPF</label>
+        <input type="tel" v-model="formData.cpf" placeholder="123.456.789-10">
+      </span>
+      <span>
+        <label for="">RG</label>
+        <input type="tel" v-model="formData.rg" placeholder="12.345.678-X">
+      </span>
+      <span>
+        <label for="">Telefone</label>
+        <input type="text" v-model="formData.telefone" placeholder="(11) 98765-4321">
+      </span>
+      <span>
+        <label for="">Email</label>
+        <input type="email" v-model="formData.email" placeholder="joao.silva@email.com">
+      </span>
+      <span class="select-field">
+        <label for="">Status do Cliente</label>
+        <select name="" id="" v-model="formData.status">
+          <option value="">Selecione</option>
+          <option value="ATIVO">Ativo</option>
+          <option value="INATIVO">Inativo</option>
+        </select>
+      </span>
+    </form>
+
+    <button 
+      class="btn-submit"
+      @click="submitForm"
+      :class="{ 'btn-disabled': !isValid() }"
+      :disabled="!isValid()"
+    >
+      Cadastrar Cliente
+    </button>
+
+  </main>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      formData: {
+        nome: '',
+        cpf: '',
+        rg: '',
+        telefone: '',
+        email: '',
+        status: ''
+      },
+      statusOptions: ['Ativo', 'Inativo']
+    };
+  },
+  methods: {
+    submitForm() {
+      event.preventDefault()
+      console.log('Dados do formulário:', this.formData);
+      this.resetForm();
+    },
+    resetForm() {
+      this.formData = {
+        nome: '',
+        cpf: '',
+        telefone: '',
+        email: '',
+        status: ''
+      };
+    },
+    isValid() {
+      for (let key in this.formData) {
+        if (!this.formData[key]) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+};
+</script>
---
+++ client/src/views/CustomerList.vue
@@ -0,0 +1,70 @@
+<template>
+    <main>
+        <h1>Meus Produtos</h1>
+        <p>
+            Cadastre novos clientes aqui,
+            você será capaz de associar produtos a ele depois.
+        </p>
+
+        <div>
+            <ListTable 
+                :headers="
+                [
+                    'Nome',
+                    'CPF',
+                    'Telefone',
+                    'Email',
+                    'Status'
+                ]" 
+                :items="clientes"
+                :actions="acoesCliente" 
+            />
+        </div>
+    </main>
+</template>
+
+<script>
+import ListTable from '@/components/ListTable.vue';
+
+export default {
+    components: {
+        ListTable
+    },
+    data() {
+        return {
+            clientes: [
+                { 
+                    id: 1,
+                    nome: 'João',
+                    cpf: '123.456.789-10',
+                    telefone: '(11) 98765-4321',
+                    email: 'email@email.com',
+                    status: 'Ativo'
+                 },
+                 { 
+                    id: 2,
+                    nome: 'João',
+                    cpf: '123.456.789-10',
+                    telefone: '(11) 98765-4321',
+                    email: 'email@email.com',
+                    status: 'Ativo'
+                 },                { 
+                    id: 3,
+                    nome: 'João',
+                    cpf: '123.456.789-10',
+                    telefone: '(11) 98765-4321',
+                    email: 'email@email.com',
+                    status: 'Ativo'
+                 },
+                // Adicione mais itens conforme necessário
+            ],
+            acoesCliente: [
+                { label: 'Editar', action: 'editar' },
+                { label: 'Excluir', action: 'excluir' },
+                { label: 'Associar', action: 'associar' },
+                // Adicione mais ações conforme necessário
+            ]
+        };
+    }
+};
+</script>

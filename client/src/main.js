11ac2fb754994a8b40a6ffca77decfebcb91593b
Thomas Almeida <thomas.almeida@tradeupgroup.com>
2024-03-06 23:54:40 -0300

    re-creating client with vite

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,14 @@
-// Plugins
-import { registerPlugins } from '@/plugins'
+import './assets/main.scss'
 
-// Components
-import App from './App.vue'
-
-// Composables
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
 
 const app = createApp(App)
 
-registerPlugins(app)
+app.use(createPinia())
+app.use(router)
 
 app.mount('#app')

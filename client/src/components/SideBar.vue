cb579a441f127b5ab052d024e07e5116f12d2562
Thomas Almeida <thomas.almeida@tradeupgroup.com>
2024-03-07 00:05:30 -0300

    Creating Sidebar component

---
+++ client/src/components/SideBar.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h3>Data Stone.<b>hub</b></h3>
+        <ul>
+            <li v-for="(option, index) in options" :key="index" @click="redirectTo(option.link)">
+                <Icon :name="option.name" />
+                <span>{{ option.option }}</span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import Icon from './Icon.vue'
+import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
+
+export default defineComponent({
+    props: {
+        options: {
+            type: Array,
+            default: () => []
+        }
+    },
+    components: {
+        Icon
+    },
+    setup() {
+        const router = useRouter()
+
+        const redirectTo = (link) => {
+            router.push(link)
+        }
+
+        return {
+            redirectTo
+        }
+    }
+})
+</script>
